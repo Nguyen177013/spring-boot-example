e10a460a57ec607cfc6dbe409dc0f304e1134b61
Nguyen177013 <phannguyendoremon@gmail.com>
2023-04-23 23:43:12 +0700

    spring boot example

---
+++ src/main/java/com/example/SpringBootExmaplpeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExmaplpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExmaplpeApplication.class, args);
+	}
+
+}
